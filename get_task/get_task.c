135e148965f0beefe01b00a8eb3297c2860c7218
Gabriele Vanoni <vanonigabriele@gmail.com>
2014-12-14 16:07:41 +0100

    Implemented get_pid

--- get_task/get_task.c
+++ get_task/get_task.c
@@ -1,13 +1,25 @@
 #include <linux/linkage.h>
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>    /* Needed for task_struct*/
 asmlinkage long sys_get_task(char __user *data, int len)
 {
     char msg [200];
+    char *str; 
+    struct task_struct *cur_task_p;
+    cur_task_p = get_current();
     if(strlen_user(data) > 200)
         return -EINVAL;
     copy_from_user(msg, data, strlen_user(data));
     printk("mysyscall: %s\n", msg);
-    copy_to_user(data, "hello world", strlen("hello world")+1);
+    str = get_pid(cur_task_p);
+    copy_to_user(data, str, 15);
     return strlen("hello world")+1;
+}
+
+char *get_pid(struct task_struct *tsk)
+{
+	char str[15];
+	sprintf(str, "%d", tsk->pid);
+	return str;
 }
