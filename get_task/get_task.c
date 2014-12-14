fc261c7a12a0402fbeabfaf18a79e303494f6148
Gabriele Vanoni <vanonigabriele@gmail.com>
2014-12-14 16:31:37 +0100

    Fixed conflict-2

--- get_task/get_task.c
+++ get_task/get_task.c
@@ -2,24 +2,25 @@
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>    /* Needed for task_struct*/
+
+void get_pid2(struct task_struct *tsk, char* str);
+
 asmlinkage long sys_get_task(char __user *data, int len)
 {
     char msg [200];
-    char *str; 
+    char str [15]; 
     struct task_struct *cur_task_p;
     cur_task_p = get_current();
     if(strlen_user(data) > 200)
         return -EINVAL;
     copy_from_user(msg, data, strlen_user(data));
     printk("mysyscall: %s\n", msg);
-    str = get_pid2(cur_task_p);
+    get_pid2(cur_task_p, str);
     copy_to_user(data, str, 15);
     return strlen("hello world")+1;
 }
 
-char *get_pid2(struct task_struct *tsk)
+void get_pid2(struct task_struct *tsk, char* str)
 {
-	char str[15];
 	sprintf(str, "%d", tsk->pid);
-	return str;
 }
